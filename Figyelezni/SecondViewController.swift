35015adece3e1cec77f3cccc49af1aa159a598d3
SaurabhSikka <drsaurabhsikka@gmail.com>
2017-07-06 00:20:53 +0200

    Initial Commit

---
+++ Figyelezni/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Figyelezni
+//
+//  Created by Saurabh Sikka on 2017. 07. 06..
+//  Copyright © 2017. Saurabh Sikka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
